85e4c27ff44700fc7df34c25290389255f23ae16
yosephwinata <yoseph.winata@gmail.com>
2024-03-12 01:04:47 +0700

    Enforce explicit function return types

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,17 +2,18 @@ module.exports = {
   root: true,
   env: { browser: true, es2020: true },
   extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:react-hooks/recommended',
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:react-hooks/recommended",
   ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
-  parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
+  ignorePatterns: ["dist", ".eslintrc.cjs"],
+  parser: "@typescript-eslint/parser",
+  plugins: ["react-refresh", "@typescript-eslint"], // Added '@typescript-eslint' to enforce explicit function return types
   rules: {
-    'react-refresh/only-export-components': [
-      'warn',
+    "react-refresh/only-export-components": [
+      "warn",
       { allowConstantExport: true },
     ],
+    "@typescript-eslint/explicit-function-return-type": ["warn"], // Added this line to enforce explicit function return types
   },
-}
+};
