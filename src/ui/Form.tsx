e05ec515e151a15d7921495c388f93911d5f08dd
yosephwinata <yoseph.winata@gmail.com>
2024-03-14 22:40:06 +0700

    Finish form

--- src/ui/Form.tsx
+++ src/ui/Form.tsx
@@ -1,22 +1,118 @@
+import { SubmitHandler, useForm } from "react-hook-form";
+import { DevTool } from "@hookform/devtools";
 import Input from "./Input";
 
+interface FormData {
+  firstName: string;
+  lastName: string;
+  email: string;
+  password: string;
+  confirmPassword: string;
+}
+
 function Form(): JSX.Element {
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+    control,
+    reset,
+  } = useForm<FormData>({
+    mode: "onBlur",
+    // defaultValues: {
+    //   firstName: "Winter",
+    //   lastName: "Bill",
+    //   email: "winterbill@gmail.com",
+    //   password: "Pakhaji5%",
+    // },
+  });
+
+  const password = watch("password");
+  // console.log("watch", watch("firstName"));
+
+  const onSubmit: SubmitHandler<FormData> = (data) => {
+    console.log(data);
+    reset();
+  };
+
   return (
-    <form className="p-6 bg-white rounded-[10px] shadow-[0_0.5rem_0_0_rgba(0,0,0,0.2)]">
-      <div className="flex flex-col gap-4">
-        <Input />
-        <Input />
-        <Input />
-        <Input />
-        <button className="bg-green h-14 rounded-[5px] text-white font-semibold text-[0.9375rem] tracking-wider shadow-green-button hover:bg-light-green hover:shadow-light-green-button">
-          CLAIM YOUR FREE TRIAL
-        </button>
-        <p className="text-[0.6875rem] leading-[1.3125rem] text-center text-grayish-blue font-medium w-[15.5625rem] mx-auto">
-          By clicking the button, you are agreeing to our{"    "}
-          <span className="text-red font-bold">Terms and Services</span>
-        </p>
-      </div>
-    </form>
+    <>
+      {/* "handleSubmit" will validate your inputs before invoking "onSubmit" */}
+      <form
+        onSubmit={handleSubmit(onSubmit)}
+        className="p-6 bg-white rounded-[10px] shadow-[0_0.5rem_0_0_rgba(0,0,0,0.2)]"
+      >
+        <div className="flex flex-col gap-4">
+          <Input
+            type="text"
+            placeholder="First Name"
+            // id="firstName"
+            // disabled={isLoading}
+            {...register("firstName", {
+              required: "First Name cannot be empty",
+            })}
+            maxLength={15}
+            error={errors.firstName?.message}
+          />
+          <Input
+            type="text"
+            placeholder="Last Name"
+            {...register("lastName", {
+              required: "Last Name cannot be empty",
+            })}
+            maxLength={15}
+            error={errors.lastName?.message}
+          />
+          <Input
+            type="email"
+            placeholder="Email Address"
+            {...register("email", {
+              required: "Email cannot be empty",
+              pattern: {
+                value: /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/,
+                message: "Please enter a valid email address",
+              },
+            })}
+            maxLength={254}
+            error={errors.email?.message}
+          />
+          <Input
+            type="password"
+            placeholder="Password"
+            {...register("password", {
+              required: "Password cannot be empty",
+              minLength: {
+                value: 8,
+                message: "Password must be at least 8 characters",
+              },
+            })}
+            error={errors.password?.message}
+          />
+          <Input
+            type="password"
+            placeholder="Confirm Password"
+            {...register("confirmPassword", {
+              required: "Confirm Password is required",
+              validate: (value) =>
+                value === password || "The passwords do not match",
+            })}
+            error={errors.confirmPassword?.message}
+          />
+          <button
+            type="submit"
+            className="bg-green h-14 rounded-[5px] text-white font-semibold text-[0.9375rem] tracking-wider shadow-green-button hover:bg-light-green hover:shadow-light-green-button"
+          >
+            CLAIM YOUR FREE TRIAL
+          </button>
+          <p className="text-[0.6875rem] leading-[1.3125rem] text-center text-grayish-blue font-medium w-[15.5625rem] mx-auto">
+            By clicking the button, you are agreeing to our{"    "}
+            <span className="text-red font-bold">Terms and Services</span>
+          </p>
+        </div>
+      </form>
+      <DevTool control={control} /> {/* set up the React Hook Form DevTools */}
+    </>
   );
 }
 
--- src/ui/Input.tsx
+++ src/ui/Input.tsx
@@ -1,10 +1,45 @@
-function Input(): JSX.Element {
-  return (
-    <input
-      placeholder="First Name"
-      className="px-5 w-full h-14 rounded-[5px] border-light-gray border-[1px] placeholder:tracking-[0.25px] placeholder:text-sm placeholder:font-semibold placeholder:text-dark-blue"
-    />
-  );
+import React from "react";
+import { UseFormRegisterReturn } from "react-hook-form";
+
+interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {
+  placeholder?: string;
+  register?: UseFormRegisterReturn;
+  error?: string;
 }
+
+const Input = React.forwardRef<HTMLInputElement, InputProps>(
+  ({ placeholder, register, error, ...rest }, ref) => {
+    return (
+      <div>
+        <div className="relative">
+          <input
+            {...register} // Spread register props if provided (React Hook Form)
+            {...rest} // Spread the rest of the props (type, id, disabled, etc.)
+            ref={ref}
+            placeholder={error ? undefined : placeholder}
+            className={`px-5 w-full h-14 rounded-[5px] border-light-gray border-[1px] placeholder:tracking-[0.25px] placeholder:text-sm placeholder:font-semibold placeholder:text-dark-blue ${
+              error && "border-red border-2"
+            }`}
+          />
+          {error && (
+            <img
+              src="/src/assets/icon-error.svg"
+              alt="Error icon"
+              className="absolute right-3 top-1/2 -translate-y-1/2 xl:right-5"
+            />
+          )}
+        </div>
+        {error && (
+          <p className="text-red text-[0.6875rem] font-medium italic mt-[0.375rem] text-right">
+            {error}
+          </p>
+        )}
+      </div>
+    );
+  }
+);
+
+Input.displayName = "Input"; // Good practice for components that use forwardRef
+// Note: When you wrap a component with React.forwardRef, the component you're defining becomes an anonymous function. Without an explicit displayName, it could appear as "ForwardRef" or similar in the React Developer Tools, depending on the version of React and the tools you're using. By setting a displayName, you ensure that the component is labeled clearly.
 
 export default Input;
